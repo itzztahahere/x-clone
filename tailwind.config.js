f4449dcc2cec40a8a5248dc6aeb7f2fa47ec5933
Muhammad Taha Sami <tahasami858@gmail.com>
2025-08-15 19:48:54 +0500

    first

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "*html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        hey: 'rgb(15 20 25 / <alpha-value>)',
+        
+      }
+      
+
+    },
+  },
+  plugins: [],
+}
